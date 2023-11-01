d00d6d0c3dc3515b81e921045235d538ad1b9c2b
dlwjiang <dlwjiang@gmail.com>
2023-11-01 14:37:57 -0700

    add images

--- shabu-shabu/src/app/page.tsx
+++ shabu-shabu/src/app/page.tsx
@@ -1,43 +1,44 @@
-import Image from 'next/image'
+import Image from "next/image";
+import { Quicksand } from "next/font/google";
+
+const font = Quicksand({ weight: "400", subsets: [] });
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <main
+      className={`${font.className} flex min-h-screen flex-col items-center justify-between p-12 bg-[radial-gradient(ellipse_at_bottom,_var(--tw-gradient-stops))] from-red-600 to-red-300`}
+    >
+      <div className="relative w-full flex flex-col items-center justify-center">
+        <Image
+          src="/shabuPot.png"
+          alt="Shabut Pot"
+          width={500}
+          height={500}
+          priority
+        />
+        <div className="absolute top-[10%]">
+          <h1
+            className={`text-white text-8xl text-center`}
+            style={{ textShadow: "4px 4px #640132" }}
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+            しゃぶしゃぶ
+          </h1>
+          <h2
+            className=" text-white text-4xl text-center"
+            // style={{ textShadow: "4px 4px #640132" }}
+          >
+            Shabu Shabu
+          </h2>
+          <h3
+            className=" text-white text-4xl text-center"
+            // style={{ textShadow: "4px 4px #640132" }}
+          >
+            A Full Pot Framework
+          </h3>
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      <div>Get Started</div>
 
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
         <a
@@ -47,7 +48,7 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
+            Docs{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
@@ -64,7 +65,7 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
+            Learn{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
@@ -81,7 +82,7 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
+            Templates{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
@@ -98,7 +99,7 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
+            Deploy{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
@@ -108,6 +109,17 @@ export default function Home() {
           </p>
         </a>
       </div>
+
+      <div>
+        Shabushabu is a "Full Pot" framework that allows you to develop using
+        typescript across the full-stack. It integrates Next.js, React, Convex
+        for a full stack typescript experience. It also setups up some common
+        useful tooling to get started easily such as Prettier, TailwindCSS, and
+        ESLint. Inspired by https://ui.shadcn.com/ philosophy of just giving you
+        code rather than yet another dependency, it has no magical moving pieces
+        and emphasizes a few high quality raw ingredients. You just fork the
+        repo and start changing things. the project.
+      </div>
     </main>
-  )
+  );
 }
